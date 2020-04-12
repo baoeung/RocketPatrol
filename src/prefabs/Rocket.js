632385a62e517345b13baae58d822f09df3dd933
Brandon <bleung7@ucsc.edu>
2020-04-12 14:49:24 -0700

    Added Collisions

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -31,4 +31,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
         }
     }
 
+    //reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = 431;
+    }
+
 }
